7682074c7c3da968534592ea1444793c684c8095
Manuel Borrero <mborrero@onturtle.eu>
2024-05-07 12:00:08 +0200

    Create prime logiche per rendere la pagina dinamica

--- js/script.js
+++ js/script.js
@@ -2,3 +2,43 @@
 
 
  //end point per key : https://strive.school/studentlogin
+
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjM5ZTFmMmQ2MzdmMzAwMTVhZGJmNTciLCJpYXQiOjE3MTUwNjk0MjYsImV4cCI6MTcxNjI3OTAyNn0.yvr-0VqDN9BogoYC_n4PqFqFpQkxttZzy69Yo014O2c';
+document.addEventListener('DOMContentLoaded', ()=>{
+
+fetch('https://striveschool-api.herokuapp.com/api/product',{
+    method:'GET',
+    headers:{
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    }
+    },
+).then(response => {
+
+    return response.json();
+}).then(data => {
+    console.log(data);
+    data.forEach(element => {
+        
+        let container = document.getElementById('container');
+        let newProduct = document.createElement('div')
+        newProduct.classList.add('card','h-20','w-20','bg-slate-600');       
+        let img = document.createElement('img');
+        img.src = element.imageUrl;
+        container.appendChild(newProduct);
+        newProduct.appendChild(img);
+        
+
+
+    });
+})
+
+})
+
+
+function createProduct(){
+
+
+
+}
+ 
